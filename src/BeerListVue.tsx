b7f78d566dc61ff64628b82b14f2261c531a0ef4
Viotty Remi <remi.viotty@etudiant.univ-rennes1.fr>
2020-09-27 16:53:56 +0200

    done list and add

--- src/BeerListVue.tsx
+++ src/BeerListVue.tsx
@@ -1,20 +1,39 @@
 import * as React from 'react';
 import { Beer } from './Beer';
 import BeerVue from './BeerVue';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 
-interface Props {
-  count?: number;
-  beers?: Beer[];
+interface Props { }
+
+interface State {
+  beers: Beer[];
+  newBeerName: string;
 }
-
-export default class BeerListVue extends React.Component<Props> {
-  static defaultProps: Props = {
-    count: 10,
-    beers: [new Beer("Coreff"), new Beer("Corona"), new Beer("Mort Subite")]
+export default class BeerListVue extends React.Component<Props, State> {
+  readonly state = {
+    beers: [new Beer("Coreff"), new Beer("Corona"), new Beer("Mort Subite")],
+    newBeerName: ""
   };
 
+  handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ newBeerName: event.target.value });
+  }
+
+  createNewBeer = () => {
+    if (this.state.newBeerName.length > 0) {
+      this.setState(state => {
+        const list = state.beers.concat(new Beer(state.newBeerName));
+        return {
+          newBeerName : "",
+          beers: list
+        };
+      });
+    }
+  }
+
   list() {
-    return this.props.beers?.map((beerItem,index) => (
+    return this.state.beers?.map((beerItem, index) => (
       <BeerVue beer={beerItem} key={index} />
     ));
   }
@@ -23,7 +42,11 @@ export default class BeerListVue extends React.Component<Props> {
     return (
       <div>
         <h1>Beer list</h1>
+        <Button variant="contained" color="primary" onClick={this.createNewBeer}>Add beer</Button>
+        <br /><br />
+        <TextField value={this.state.newBeerName} label="Name of the beer" variant="outlined" onChange={this.handleChange} />
         <ul>{this.list()}</ul>
+
       </div>
     );
   }
--- src/BeerVue.tsx
+++ src/BeerVue.tsx
@@ -1,5 +1,8 @@
 import * as React from 'react';
 import { Beer } from './Beer';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
 
 interface Props {
   beer: Beer
@@ -9,6 +12,16 @@ export default class BeerVue extends React.Component<Props> {
 
 
   render() {
-    return <li><h2>{this.props.beer.name}</h2></li>;
+
+    return (
+      <li>
+        <Card>
+          <CardContent>
+            <Typography variant="h5" component="h2">
+              {this.props.beer.name}
+            </Typography>
+          </CardContent>
+        </Card>
+      </li>);
   }
 }
